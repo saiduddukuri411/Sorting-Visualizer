a00410a4192f852ab77fdb3570ad540dc0fcc177
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-06-22 16:38:45 -0400

    done with the header

--- visualizer/src/app.js
+++ visualizer/src/app.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from './pages/Header/frame';
 
 const App = () => {
+    const [type,setType]=React.useState('Bubble')
     return (
         <>
-         <Header />   
+         <Header sortType={type} setType={setType}/>   
         </>
     )
 }
--- visualizer/src/pages/Header/frame.js
+++ visualizer/src/pages/Header/frame.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import './styles/frame.scss';
-import {FaPlay} from 'react-icons/fa';
-import {AiOutlineReload,AiFillCaretDown} from 'react-icons/ai';
-const types=['Bubble','Merge', 'Quick','Insertion']
-const Headerframe = () => {
-    return (
-        <header className="headerContainer">
-            <h1><span className="left">SORT</span> IT</h1>
-            <div class="headerIcons">
-                <FaPlay className="icons"/>
-                <AiOutlineReload className="icons"/>
-            </div>
-            <div className="headerDropdown">
-                <h2>Bubble Sort</h2>
-                <AiFillCaretDown className="downArrow"/>
-                <div className="content">
-                    {types.map(type=>(
-                      <h7 key={type}>{type}</h7>
-                    ))}
-                    
-                </div>
-            </div>
-        </header>
-    )
-}
+import React from "react";
+import "./styles/frame.scss";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlineReload, AiFillCaretDown } from "react-icons/ai";
+const types = ["Bubble", "Merge", "Quick", "Insertion"];
+const Headerframe = React.memo(({ sortType, setType}) => {
+  const [count,setCount ]=React.useState(0)
+  return (
+    <header className="headerContainer">
+      <h1>
+        <span className="left">SORT</span> IT
+      </h1>
+      <div className="headerIcons">
+        <FaPlay className="icons" />
+        <AiOutlineReload className="icons" />
+      </div>
+      <div
+        className="headerDropdown"
+        onClick={()=>{setCount(prev=>prev+1)}}
+      >
+        <h2>{`${sortType} Sort`}</h2>
+        <AiFillCaretDown className="downArrow" />
+        {count%3!==0 ? (
+          <div className="content">
+            {types.map((type) => (
+              <h7
+                key={type}
+                onClick={()=>{
+                  setType((prev) => type);
+                  setCount(prev=>prev+1)
+                }}
+              >
+                {type}
+              </h7>
+            ))}
+          </div>
+        ) : null}
+      </div>
+    </header>
+  );
+});
 export default Headerframe;
